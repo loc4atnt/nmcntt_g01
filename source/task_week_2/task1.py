051173613a00d784ace2d53330e50ae9a50dbad4
tam18902 <75715241+tam18902@users.noreply.github.com>
2020-12-16 22:38:14 +0700

    Update task1.py

--- source/task_week_2/task1.py
+++ source/task_week_2/task1.py
@@ -2,5 +2,11 @@ import numpy as np
 
 # Parameter: img is a numpy array with shape (x,y)
 # Return: a numpy array with shape (x*y)
-def imgToVector(img):
-	return np.array([]) # Fake return
+def imgToVector(array2d):
+	r = array2d.shape[0]
+	c = array2d.shape[1]
+	array1d = np.zeros(c*r,dtype=int)
+	for i in range(r):
+		for j in range(c):
+			array1d[i*c+j] = array2d[i, j]
+	return array1d # Fake return
