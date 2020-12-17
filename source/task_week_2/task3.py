1f19f0d53f33681692c1cae5e799d4115167f523
manhh15 <75713644+manhh15@users.noreply.github.com>
2020-12-17 11:43:09 +0700

    Update task3.py

--- source/task_week_2/task3.py
+++ source/task_week_2/task3.py
@@ -1,7 +1,18 @@
 import numpy as np
-def Get_Average(arr):
+def avgArr(arr):
     return np.mean(arr)
-def Get_Max(arr):
-    return np.max(arr)
-def Get_Min(arr):
+def minArr(arr):
     return np.min(arr)
+def maxArr(arr):
+    return np.max(arr)
+
+# define --------
+AVG = 0
+MIN = 1
+MAX = 2
+#----------------
+
+def getArrSamplingVal(arr, option):
+    if option == 0 : return avgArr(arr)
+    if option == 1 : return minArr(arr)
+    if option == 2 : return maxArr(arr)
