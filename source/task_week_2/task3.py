cb6aba4f437ce22e9ee8c6e608daa5c9be804f2a
manhh15 <75713644+manhh15@users.noreply.github.com>
2020-12-17 09:31:06 +0700

    Update task3.py

--- source/task_week_2/task3.py
+++ source/task_week_2/task3.py
@@ -1,26 +1,7 @@
-
-# Parameter: arr is a numpy array with shape (x,y)
-# Return: a number
-def avgArr(arr):
-	return 0 # Fake return
-
-# Parameter: arr is a numpy array with shape (x,y)
-# Return: a number
-def minArr(arr):
-	return 0 # Fake return
-
-# Parameter: arr is a numpy array with shape (x,y)
-# Return: a number
-def maxArr(arr):
-	return 0 # Fake return
-
-# === SAMPLING KIND === #
-AVG = 0
-MIN = 1
-MAX = 2
-# ===================== #
-# Parameter: arr is a numpy array with shape (x,y)
-#			 option is a number in SAMPLING KIND
-# Return: a number
-def getArrSamplingVal(arr, option):
-	return 0 # Fake return
+import numpy as np
+def Get_Average(arr):
+    return np.mean(arr)
+def Get_Max(arr):
+    return np.max(arr)
+def Get_Min(arr):
+    return np.min(arr)
