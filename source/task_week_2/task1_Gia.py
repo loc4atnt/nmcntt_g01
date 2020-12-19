bd1ee9297f2f2f5c972cd9bb63943839e6af118b
Gia Huynh <huynhthietgia123@gmail.com>
2020-12-20 02:18:24 +0700

    Updated Model + cleaned Task 1
    Sang mai update lai model moi tiep, vjp hon vai phan tram, model nay su dung duoc roi.

--- source/task_week_2/task1_Gia.py
+++ source/task_week_2/task1_Gia.py
@@ -1,4 +1,20 @@
+
+def prepare_dataset():
+    #load dataset
+    global X_train, X_test, y_train, y_test
+    X_train, y_train = loadMnist("../data/")
+    X_test, y_test = loadMnist("../data/", kind='test')
+    #chia 255, pixel value tu [0, 255] sang [0, 1]
+    X_train = X_train/255.0
+    X_test = X_test/255.0
+    #format lai input cho phu hop keras
+    X_train = X_train[..., np.newaxis]
+    X_test = X_test[..., np.newaxis]
+    y_train = tf.keras.utils.to_categorical (y_train)
+    y_test = tf.keras.utils.to_categorical (y_test)
+    
 def build_model():
+    #define kich co anh
     size = 28
     inputs = Input((size, size, 1))
 
@@ -27,39 +43,33 @@ def build_model():
     return Model(inputs, x)
 
 def train_model():
-    opt = tf.keras.optimizers.SGD(0.001)
+    opt = tf.keras.optimizers.SGD(0.0005)
     metrics = ["acc"]
     model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=metrics)
-    for i in range (0, 5):
+    #Train 20 lan, moi lan 20 epoch.
+    for i in range (0, 20):
         model.fit (X_train, y_train, batch_size=32, epochs=20, verbose=2, validation_data=(X_test, y_test))
         model.save ("gaymodel2.h5")
 
+    
 import os
 from main_Gia_modified import *
 import tensorflow as tf
-
 #Dung neu tensorflow an het vram cua GPU
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
   tf.config.experimental.set_memory_growth(gpu, True)
-
-#load + format lai dataset
-X_train, y_train = loadMnist("../data/")
-X_test, y_test = loadMnist("../data/", kind='test')
-#chia 255, pixel value tu [0, 255] sang [0, 1]
-X_train = X_train/255.0
-X_test = X_test/255.0
-X_train = X_train[..., np.newaxis]
-X_test = X_test[..., np.newaxis]
-y_train = tf.keras.utils.to_categorical (y_train)
-y_test = tf.keras.utils.to_categorical (y_test)
-
+  
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.initializers import random_normal
+
+prepare_dataset()
+#uncomment de tao model moi
+#model = build_model()
+#load model cu len, neu tao model moi thi comment dong nay
 model = load_model('gaymodel2.h5')
-#uncomment hai dong nay de train model
-model = build_model()
+#Train model
 train_model()
 
 #output cua model2 la numpy array size: (1, 256)
