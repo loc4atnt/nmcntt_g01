5d50dd4cca51818c61eb4edf8dbc15be5e4e488b
Le Quang Tri <49644142+trislee02@users.noreply.github.com>
2020-12-19 12:30:40 +0700

    Update task2.py

--- source/task_week_2/task2.py
+++ source/task_week_2/task2.py
@@ -2,7 +2,11 @@ import numpy as np
 
 import task1
 
-# Parameter: imgArr is a numpy array with shape (n,x,y)
+# Parameter: imgArr is a numpy array with shape (n,x,y) - with n is the number of images
 # Return: a numpy array with shape (n, x*y)
 def multiImgToVector(imgArr):
+	n = imgArr.shape[0]
+	arrImgVec = np.zeros((n,imgArr.shape[1]*imgArr.shape[2]))
+	for i in range(n):
+		arrImgVec[i] = imgToVector(imgArr[i])
 	return np.array([[]]) # Fake return
