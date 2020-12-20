619b129269002c8a146242581a478b61216cf68b
Gia Huynh <huynhthietgia123@gmail.com>
2020-12-20 14:05:54 +0700

    Revert "Task 1 Done"
    This reverts commit 3b516d7d7d134246622ce3ff6558675ecb911d1b.

--- source/task_week_2/task1.py
+++ source/task_week_2/task1.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 # Parameter: img is a numpy array with shape (x,y)
 # Return: a numpy array with shape (x*y)
 def imgToVector(array2d):
@@ -9,12 +10,3 @@ def imgToVector(array2d):
 		for j in range(c):
 			array1d[i*c+j] = array2d[i, j]
 	return array1d # Fake return
-
-from main_Gia_modified import *
-
-X_train, y_train = loadMnist("../data/")
-X_test, y_test = loadMnist("../data/", kind='test')
-
-X_train_vectorized = np.zeros ((X_train.shape[0], X_train[0].shape[0]*X_train[0].shape[1]))
-for i in range (0, X_train.shape[0]):
-        X_train_vectorized [i] = imgToVector(X_train[i])
